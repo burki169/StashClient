3b38b54898f80834c3c7f2f53caaf98005e310b2
Andres Gil Herrera <andres.gil.herrera@iav.de>
2018-08-16 12:38:15 +0200

    Added support for some Rest API methods and added the model classes for their results.

---
+++ Objects/StashTagList.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StashClient.Objects
+{
+    public partial class StashTagList
+    {
+        [JsonProperty("isLastPage")]
+        public bool IsLastPage { get; set; }
+
+        [JsonProperty("size")]
+        public long Size { get; set; }
+
+        [JsonProperty("values")]
+        public Tag[] Tags { get; set; }
+
+        [JsonProperty("limit")]
+        public long Limit { get; set; }
+
+        [JsonProperty("start")]
+        public long Start { get; set; }
+
+        public static StashTagList FromJson(string json) => JsonConvert.DeserializeObject<StashTagList>(json, Converter.Settings);
+
+    }
+}
